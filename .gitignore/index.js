a12217e7c36a689c0489ad4e43a43568558ad25f
Jordan WAILAND <34924111+midnightskywailand@users.noreply.github.com>
2017-12-28 22:40:52 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,12 +1,12 @@
-const Discord = require("discord.js");
+#const Discord = require("discord.js");
 
-var bot = new Discord.Client();
+#var bot = new Discord.Client();
 var prefix = ("/");
 
-bot.on("ready", function()  {
-    bot.user.setGame("Faggot Never Die");
-    console.log("Bot Ready");
-})
+#bot.on("ready", function()  {
+    #bot.user.setGame("Faggot Never Die");
+    #console.log("Bot Ready");
+#})
 
 bot.login("MzcxMzYyMTQyMjIzNDY2NDk2.DSb0Gw.r8xoTLHoiKSQgNCK-fCTkEzxJpM");
 
